2684921a762d1f78790743d3af5f467432247f70
yumkea <152074213+yumkea@users.noreply.github.com>
2024-03-10 11:46:43 +0800

    Delete OneRoad directory

--- OneRoad/src/BackEnd0001/InsufficientBalanceException.java
+++
@@ -1,9 +0,0 @@
-package BackEnd0001;
-
-public class InsufficientBalanceException extends Exception{
-    private String message;
-    public InsufficientBalanceException(String accountId,int pendingAmount){
-        super("余额不足! 账户号码为:"+accountId+"尝试提取的金额为:"+pendingAmount);
-        this.message="账户号码为:"+accountId+"尝试提取的金额为:"+pendingAmount;
-    }
-}
